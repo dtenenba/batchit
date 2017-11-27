8ab99409cf1268733b318a13321bb3f997d7235e
Dan Tenenbaum <dtenenba@fhcrc.org>
2017-11-27 08:36:30 -0800

    add test script

---
+++ cmd/batchit/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo hello from batchit
+
+# example cmdline:
+#  batchit submit --image ubuntu:16.04 --role fh-pi-gottardo-r-batchtask --region us-west-2 --queue mixed --cpus 1 --mem 500 --jobname dtenenba-batchit-test-0 ./test.sh
